5cc2f65af6c5eb453ea9f1f58eef543cd6de03fb
Janvi <jg.gupta.janvi@gmail.com>
2023-04-05 15:22:49 +0530

    Added my project

---
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -0,0 +1,105 @@
+package com.example.justjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.NumberFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+    int quantity = 2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void increment(View view){
+        if (quantity == 100) {
+            Toast.makeText(this, "You cannot have more than 100 coffees", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        quantity = quantity + 1;
+        displayQuantity(quantity);
+    }
+    public void decrement(View view){
+        if (quantity == 1) {
+            // Show an error message as a toast
+            Toast.makeText(this, "You cannot have less than 1 coffee", Toast.LENGTH_SHORT).show();
+            // Exit this method early because there's nothing left to do
+            return;
+        }
+        quantity = quantity - 1;
+        displayQuantity(quantity);
+    }
+
+
+    @SuppressLint("QueryPermissionsNeeded")
+    public void submitOrder(View view){
+        // Find the user's name
+        EditText nameField = findViewById(R.id.name_field);
+        Editable nameEditable = nameField.getText();
+        String name = nameEditable.toString();
+
+         // Figure out if the user wants whipped cream topping
+        CheckBox whippedCreamCheckBox = findViewById(R.id.whipped_cream_checkbox);
+        boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
+
+          //Figure out if the user wants chocolate topping
+        CheckBox chocolateCheckBox = findViewById(R.id.chocolate_checkbox);
+        boolean hasChocolate = chocolateCheckBox.isChecked();
+
+        int price = calculatePrice(hasWhippedCream,hasChocolate);
+        String message = createOrderSummary(name,price, hasWhippedCream,hasChocolate);
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:"));
+        intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.order_summary_email_subject)+ name);
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+        if(intent.resolveActivity(getPackageManager()) != null){
+            startActivity(intent);
+        }
+    }
+
+
+    private int calculatePrice(boolean hasWhippedCream, boolean hasChocolate){
+        int basePrice = 5;
+        if(hasWhippedCream){
+            basePrice+=1;
+        }
+        if(hasChocolate){
+            basePrice+=2;
+        }
+        return quantity * basePrice;
+    }
+
+    private String createOrderSummary(String name,int price, boolean addWhippedCream, boolean addChocolate){
+        String priceMessage = getString(R.string.order_summary_name,name);
+        priceMessage += "\n" + getString(R.string.order_summary_whipped_cream,addWhippedCream);
+        priceMessage += "\n" + getString(R.string.order_summary_chocolate,addChocolate);
+        priceMessage+= "\n" + getString(R.string.order_summary_quantity, quantity);
+        priceMessage += "\n" + getString(R.string.order_summary_price, NumberFormat.getCurrencyInstance().format(price));
+        priceMessage += "\n" + getString(R.string.thank_you);
+        return priceMessage;
+    }
+
+    // This method displays the given quantity value on the screen
+    @SuppressLint("SetTextI18n")
+    private void displayQuantity(int numberOfCoffees){
+        TextView quantityTextView = findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + numberOfCoffees);
+        quantityTextView.setText("" + numberOfCoffees);
+    }
+
+
+}
